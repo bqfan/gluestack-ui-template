65a5a369836afabc059752cd9220c8d5fc34c45f
Baiquan Fan <baiquanf.dev@gmail.com>
2025-02-04 21:59:54 +0200

    chore(gluestack-ui): add gluestack-ui configuration

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to My App!</Text>
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="text-blue-600 dark:text-sky-400">Welcome to My App!</Text>
     </View>
   );
 }
